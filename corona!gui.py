646553c95dadb3fc4f9521e940f2ec3a3bc859bc
Kartik Thakkar <ktstudy999@gmail.com>
2020-04-15 18:35:06 +0530

    Simple Python script

---
+++ corona!gui.py
@@ -0,0 +1,20 @@
+import COVID19Py
+kt=COVID19Py.COVID19()
+def show(x):
+    cd=x
+    code=kt.getLocationByCountryCode(cd)
+    ld=code[0]
+    vd=dict(ld['latest'])
+    print("Country Name = "+ld['country']+","+ld['country_code']+"\n")
+    print("Country Population = "+str(ld['country_population'])+"\n")
+    print("Confirmed = "+str(vd['confirmed'])+"\n")
+    print("Deaths = "+str(vd['deaths'])+"\n")
+    print("Recovered = "+str(vd['recovered'])+"\n")
+    lu=ld['last_updated']
+    print("Last Updated = "+lu[0:10]+"\n")
+
+
+print("First Find Country code by going to this link \n https://www.willmaster.com/blog/misc/country-name-abbreviations.php ")
+print("Enter Country Name: \n Eg:- India = In \n united States = Us \n United Kingdom = Gb \n")
+x=input()
+show(x)
